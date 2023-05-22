785c9473a1879b9acae84282f384fc4ffd4b31d3
CHERCH06 <35665611+CHERCH06@users.noreply.github.com>
2023-05-22 15:07:00 -0600

    Agregar Setter y Getter
    Setter & Getter

--- NetBeansProjects/TiendaComputadoras/src/tiendacomputadoras/Main.java
+++ NetBeansProjects/TiendaComputadoras/src/tiendacomputadoras/Main.java
@@ -47,5 +47,54 @@ class Tienda{
 }
 
 class Computadora{
+    String idC,marca,mem,proc,so,prec;
+
+    public String getIdC() {
+        return idC;
+    }
+
+    public void setIdC(String idC) {
+        this.idC = idC;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getMem() {
+        return mem;
+    }
+
+    public void setMem(String mem) {
+        this.mem = mem;
+    }
+
+    public String getProc() {
+        return proc;
+    }
+
+    public void setProc(String proc) {
+        this.proc = proc;
+    }
+
+    public String getSo() {
+        return so;
+    }
+
+    public void setSo(String so) {
+        this.so = so;
+    }
+
+    public String getPrec() {
+        return prec;
+    }
+
+    public void setPrec(String prec) {
+        this.prec = prec;
+    }
     
 }
