5c1d4bf73c764bf2a862a91a97960242d7db043c
CHERCH06 <35665611+CHERCH06@users.noreply.github.com>
2023-05-24 21:25:10 -0600

    Clase Tienda
    Se implemento los metodos set y get de sus atributos y se creo el constructor

--- TiendaHerencia/src/tiendaherencia/Main.java
+++ TiendaHerencia/src/tiendaherencia/Main.java
@@ -10,6 +10,37 @@ public class Main {
 }
 
 class Tienda{
+    private String nombre,dueño,id;
+
+    public Tienda(String nombre, String dueño, String id) {
+        this.nombre = nombre;
+        this.dueño = dueño;
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDueño() {
+        return dueño;
+    }
+
+    public void setDueño(String dueño) {
+        this.dueño = dueño;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
     
 }
 
