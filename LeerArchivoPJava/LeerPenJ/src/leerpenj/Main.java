08c5d079a9994917c025ef6e607395187af4f6a1
CHERCH06 <SERGIOXD618@GMAIL.COM>
2023-10-02 20:45:31 -0600

    CrwearProyectoJava
    Leer arcvhicvo de python en java

---
+++ LeerArchivoPJava/LeerPenJ/src/leerpenj/Main.java
@@ -0,0 +1,10 @@
+package leerpenj;
+
+/* @author VGSG */
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
--- PhytonProjects/PythonGenerarArchivo/main.py
+++ PhytonProjects/PythonGenerarArchivo/main.py
@@ -8,7 +8,7 @@ def Hora():
     return auxiliar
 
 id = 1
-titulos = "ID_TRABAJO|HORA_LLEGADA|TIEMPO_CPU|PRIORIDAD"
+titulos = "ID_TRABAJO|HORA_LLEGADA|TIEMPO_CPU|PRIORIDAD\n"
 proyeMin = 20
 proyeMax = 100
 prioriMin = 1
@@ -20,6 +20,7 @@ proceMax = 4
 procesa = random.randint(proceMin,proceMax)
 proye = random.randint(proyeMin,proyeMax)
 proyectos = []
+proyectos.append(titulos)
 for n in range(proye):
     aux = " "+str(id).zfill(2)+"|"
     aux = aux + str(Hora())+"|"
