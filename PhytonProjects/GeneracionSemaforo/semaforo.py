f44c754052d459435b116be2c2780fa5423f0248
DaniMG13 <dandex0813@gmail.com>
2023-10-17 21:52:48 -0600

    update 2
    Se añadieron 2 semáforos más que trabajan de forma independiente. Además, se le agregó un fondo de color a cada semáforo. Falta corregir los tiempos de las luces de los semáforos

--- PhytonProjects/GeneracionSemaforo/semaforo.py
+++ PhytonProjects/GeneracionSemaforo/semaforo.py
@@ -1,52 +1,77 @@
 import tkinter as tk
 import time
+import threading
 
-# Función para cambiar el estado del semáforo
-def cambiar_estado():
-    canvas.itemconfig(luz_roja, fill="red")
-    canvas.itemconfig(luz_amarilla, fill="gray")
-    canvas.itemconfig(luz_verde, fill="gray")
-    mensaje.set("Semáforo: Rojo")
-    root.update()
-    time.sleep(30)  # Espera 2 segundos
-    canvas.itemconfig(luz_roja, fill="gray")
-    canvas.itemconfig(luz_verde, fill="green")
-    mensaje.set("Semáforo: Verde")
-    root.update()
-    time.sleep(33)  # Espera 2 segundos
-    canvas.itemconfig(luz_verde, fill="gray")
-    canvas.itemconfig(luz_amarilla, fill="yellow")
-    mensaje.set("Semáforo: Amarillo")
-    root.update()
-    time.sleep(2)  # Espera 1 segundo
-    cambiar_estado()  # Llama recursivamente para hacer un ciclo
+# Función para cambiar el estado de un semáforo
+def cambiar_estado(semaforo, tiempo_rojo, tiempo_verde, tiempo_amarillo):
+    while True:
+        semaforo.itemconfig(luz_roja, fill="red")
+        semaforo.itemconfig(luz_amarilla, fill="gray")
+        semaforo.itemconfig(luz_verde, fill="gray")
+        mensaje.set("Semáforo: Rojo")
+        root.update()
+        time.sleep(tiempo_rojo)
+        
+        semaforo.itemconfig(luz_roja, fill="gray")
+        semaforo.itemconfig(luz_verde, fill="green")
+        mensaje.set("Semáforo: Verde")
+        root.update()
+        time.sleep(tiempo_verde)
+        
+        semaforo.itemconfig(luz_verde, fill="gray")
+        semaforo.itemconfig(luz_amarilla, fill="yellow")
+        mensaje.set("Semáforo: Amarillo")
+        root.update()
+        time.sleep(tiempo_amarillo)
 
 # Crear la ventana
 root = tk.Tk()
-root.title("Semáforo Peatones")
+root.title("Semáforos Peatones")
 
-# Crear un título para el semáforo
-titulo = tk.Label(root, text="Semáforo Peatones")
+# Crear un título para los semáforos
+titulo = tk.Label(root, text="Semáforos Peatones")
 titulo.pack()
 
 # Crear una variable de control para el mensaje
 mensaje = tk.StringVar()
 mensaje.set("Semáforo: Apagado")
 
-# Crear el lienzo (canvas) para el semáforo
-canvas = tk.Canvas(root, width=100, height=300)
-canvas.pack()
-
-# Dibujar las luces del semáforo
-luz_roja = canvas.create_oval(30, 30, 70, 70, fill="gray")
-luz_amarilla = canvas.create_oval(30, 110, 70, 150, fill="gray")
-luz_verde = canvas.create_oval(30, 190, 70, 230, fill="gray")
-
 # Crear etiqueta (label) para mostrar el mensaje
 label = tk.Label(root, textvariable=mensaje)
 label.pack()
 
-# Iniciar el semáforo
-cambiar_estado()
+# Crear un contenedor para los semáforos (frame vertical)
+semaforos_container = tk.Frame(root)
+semaforos_container.pack()
+
+# Crear el lienzo (canvas) para el primer semáforo (vertical)
+canvas1 = tk.Canvas(semaforos_container, width=100, height=300, bg="gray")
+canvas1.grid(row=0, column=0)
+luz_roja = canvas1.create_oval(30, 30, 70, 70, fill="gray")
+luz_amarilla = canvas1.create_oval(30, 110, 70, 150, fill="gray")
+luz_verde = canvas1.create_oval(30, 190, 70, 230, fill="gray")
+
+# Crear el lienzo (canvas) para el segundo semáforo (vertical)
+canvas2 = tk.Canvas(semaforos_container, width=100, height=300, bg="gray")
+canvas2.grid(row=0, column=1)
+luz_roja2 = canvas2.create_oval(30, 30, 70, 70, fill="gray")
+luz_amarilla2 = canvas2.create_oval(30, 110, 70, 150, fill="gray")
+luz_verde2 = canvas2.create_oval(30, 190, 70, 230, fill="gray")
+
+# Crear el lienzo (canvas) para el tercer semáforo (vertical)
+canvas3 = tk.Canvas(semaforos_container, width=100, height=300, bg="gray")
+canvas3.grid(row=0, column=2)
+luz_roja3 = canvas3.create_oval(30, 30, 70, 70, fill="gray")
+luz_amarilla3 = canvas3.create_oval(30, 110, 70, 150, fill="gray")
+luz_verde3 = canvas3.create_oval(30, 190, 70, 230, fill="gray")
+
+thread1 = threading.Thread(target=cambiar_estado, args=(canvas1, 30, 33, 2))
+thread2 = threading.Thread(target=cambiar_estado, args=(canvas2, 20, 25, 3))
+thread3 = threading.Thread(target=cambiar_estado, args=(canvas3, 15, 35, 5))
+
+# Iniciar los hilos
+thread1.start()
+thread2.start()
+thread3.start()
 
 root.mainloop()
